1da09700ff0fa13b0b7872d0f016848435dc9e42
AshrafHamayel <78316328+AshrafHamayel@users.noreply.github.com>
2022-07-24 14:29:13 +0300

    update

--- lib/api.dart
+++ lib/api.dart
@@ -1,11 +1,9 @@
 // ignore_for_file: unused_import, depend_on_referenced_packages, camel_case_types
-
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
-
 import 'package:flutter/material.dart';
 import 'package:graduate_proj/posts.dart';
+import 'signup.dart';
 
 class api {
   static Future<http.Response> createUser(
